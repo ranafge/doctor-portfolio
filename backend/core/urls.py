51a1adbb25ef80151c69a01907f59d13f242a2bd
ranafge <ranafge@gmail.com>
2026-06-15 21:49:30 +0600

    main url settings update

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -35,4 +35,8 @@ urlpatterns = [
    
 ] 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Render-এ প্রোডাকশনের জন্যও এটি যোগ করুন
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
